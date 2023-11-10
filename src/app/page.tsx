930230c3a3befd3b31d8a9c17f32dc5efcde8385
Sandeep Suresh <sandeepsuresh@Sandeeps-MacBook-Air.local>
2023-11-10 17:07:35 -0500

    Theme picked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { useState } from 'react'
-
+import styles from "./page.module.css";
 import AudioRecorder from './components/AudioRecorder';
 
 export default function Home() {
 
   const isRecording = useState(false)
   return (
-    <main>
+    <main className={styles.main}>
       <AudioRecorder />
     </main>
   )
