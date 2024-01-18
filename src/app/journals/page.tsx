6774bba422295136cf4de6029a6a332e539b8c89
Sandeep Suresh <sandeepsuresh98@gmail.com>
2024-01-18 12:26:31 -0500

    Adding Submission

--- src/app/journals/page.tsx
+++ src/app/journals/page.tsx
@@ -1,19 +1,36 @@
 'use client';
-import React from "react";
-import TipTap from '../components/TipTap';
+import React, { useState } from "react";
+// import TipTap from '../components/TipTap'; TODO: Add when ready
 import styles from "../page.module.css";
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
-
-
+import axios from 'axios';
 
 export default withPageAuthRequired(
   function Journals() {
     const { user, error, isLoading } = useUser();
+    const [text, setText] = useState('');
+    console.log(user?.sub);
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      try {
+        const response = await axios.post('http://localhost:8000/v1/embeddings/create', { 
+          'user_id': user?.sub,
+          'text': text,
+        });
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
     return (
       <main className={styles.main}>
         Hello {user?.name}
-
-        <TipTap />
+        <form onSubmit={handleSubmit}>
+          <textarea value={text} onChange={e => setText(e.target.value)} />
+          <button type="submit">Submit</button>
+        </form>
       </main>
     )
   }
